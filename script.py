142ddf131aa479c53f668aa9dcd5946df08e3c70
Anna-Lagunova <91545553+Anna-Lagunova@users.noreply.github.com>
2025-10-09 13:08:04 +0300

    Update script.py

--- script.py
+++ script.py
@@ -1,7 +1,6 @@
 import telebot
 from telebot import types
 
-# Замените 'YOUR_BOT_TOKEN' на токен вашего бота
 bot = telebot.TeleBot('8411699454:AAFtArqY1tFgz89qGK18JmWpMEbfXtNJihY')
 
 # Обработчик команды /start
@@ -11,15 +10,15 @@ def send_welcome(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
     
     # Создаем кнопки
-    btn1 = types.KeyboardButton('завтрак')
-    btn2 = types.KeyboardButton('обед')
-    btn3 = types.KeyboardButton('ужин')
+    btn1 = types.KeyboardButton('Завтрак')
+    btn2 = types.KeyboardButton('Обед')
+    btn3 = types.KeyboardButton('Ужин')
     
     # Добавляем кнопки в клавиатуру
     markup.add(btn1, btn2, btn3)
     
     # Отправляем сообщение с клавиатурой
-    bot.send_message(message.chat.id, "Выберите прием пищи:", reply_markup=markup)
+    bot.send_message(message.chat.id, "Что готовим?", reply_markup=markup)
 
 # Обработчик текстовых сообщений
 @bot.message_handler(func=lambda message: True)
@@ -30,25 +29,43 @@ def handle_message(message):
         
         # Список блюд для завтрака
         breakfast_dishes = [
-            "блинчики", "сырники", "оладушки", "яичница",
-            "рисовая каша", "омлет", "овсяная каша", "вафли",
-            "скрэмбл", "манная каша", "творог", "гречневая каша", "шакшука"
+            "Блины", "Сырники", "Оладья", "Яичница",
+            "Рисовая каша", "Омлет", "Овсяная каша", "Вафли",
+            "Скрэмбл", "Манная каша", "Творог", "Гречневая каша", "Шакшука"
         ]
         
         # Создаем кнопки для каждого блюда
         for dish in breakfast_dishes:
             markup.add(types.KeyboardButton(dish))
-        
+
+        # Список блюд для обеда
+        lunch_dishes = [
+            "Минестроне", "Чечевичный суп", "Окрошка", "Врап",
+            "Щавелевый суп", "Шаверма", "Гороховый суп", "Рыбный суп",
+            "Томатный суп", "Картофельный крем-суп", "Салат"
+        ]
+        # Создаем кнопки для каждого блюда
+        for dish in lunch_dishes:
+            markup.add(types.KeyboardButton(dish))
+
+        # Список блюд для ужина
+        dinner_dishes = [
+            "Запеченная рыба", "Запеченное мясо", "Тушеная капуста", "Плов",
+            "Болоньезе", "Шаверма", "Драники", "Соус томатный",
+            "Котлеты", "Вафли", "Салат", "Пастуший пирог", "Лобио", "Оливье", "Поке", "Вок", "Стейк"
+        ]
+        # Создаем кнопки для каждого блюда
+        for dish in dinner_dishes:
+            markup.add(types.KeyboardButton(dish))
         # Добавляем кнопку "Назад"
         markup.add(types.KeyboardButton("Назад"))
         
         bot.send_message(message.chat.id, "Выберите блюдо на завтрак:", reply_markup=markup)
     
-    elif message.text.lower() == 'обед':
-        bot.send_message(message.chat.id, "Функционал для обеда в разработке")
     
-    elif message.text.lower() == 'ужин':
-        bot.send_message(message.chat.id, "Функционал для ужина в разработке")
+        bot.send_message(message.chat.id, "Выберите блюдо на обед:", reply_markup=markup)   
+    
+        bot.send_message(message.chat.id, "Выберите блюдо на ужин:", reply_markup=markup)
     
     elif message.text.lower() == 'назад':
         # Возвращаемся к главному меню
